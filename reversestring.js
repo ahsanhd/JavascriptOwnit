c66b8bc12e9b81e69482fa5b118051d1c516dd98
M Ahsan Sandhu <ahsanhd@outlook.com>
2025-07-11 17:52:08 +0500

    Reverse a string porblem solution

---
+++ reversestring.js
@@ -0,0 +1,24 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+const reverseString = (string) => {
+  let rArray = [];
+  const straightArray = string.split("");
+  for (let i = straightArray.length; i >= 0; i--) {
+    rArray.push(straightArray[i - 1]);
+  }
+  return rArray.join("");
+};
+const result = reverseString("Ahsan");
+
+console.log({ result_: result });
+// using reduce methoh
+const reverseString_ = (string) => {
+  return string.split("").reduce((acc, char) => {
+    return char + acc;
+  }, "");
+};
+
+console.log("reversed string", reverseString_("ahsan"));
