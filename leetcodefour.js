79eb478531792812249d6f4efa5d1e7092daffd3
M Ahsan Sandhu <ahsanhd@outlook.com>
2025-07-21 21:02:13 +0500

    leet code reverse the vowels two pointers and while loop

---
+++ leetcodefour.js
@@ -0,0 +1,50 @@
+/* Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+
+Input: s = "leetcode"
+
+Output: "leotcede" */
+
+/* Solution  */
+
+/**
+ * @param {string}
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  const arr = s.split("");
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    while (left < right && !vowels.has(arr[left])) {
+      left++;
+    }
+    while (left < right && !vowels.has(arr[right])) {
+      right--;
+    }
+
+    // Swap vowels
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr.join("");
+};
